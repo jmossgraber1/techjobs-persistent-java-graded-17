9892a71b0168dc7c6810632392b9713abe23724e
Jen Moss <jennmossgraber@gmail.com>
2023-12-13 16:13:46 -0600

    GA 4 12/13, 4pm

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,25 +1,36 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Employer extends AbstractEntity {
+    public Employer(){}
 
 
-    public void setLocation(String location) {
-        this.location = location;
+
+    @OneToMany(mappedBy = "employer")
+    @JoinColumn()
+    private final List<Job> jobs = new ArrayList<>();
+    public List<Job> getJobs() {
+        return jobs;
     }
+
+// TODO: OneToMany needs a name parameter - what should it be?
 
     @NotBlank(message = "Employer is required")
     @Size(min = 3, max = 200)
     private String location;
-    public String getLocation() {
-        return location;}
-    public Employer(){}
+    public String getLocation() {return location;}
+    public void setLocation(String location) {this.location = location;}
 
-    //1. add string for location
-    //2. limit location to one instance ***HOW TO?
-    //3. att @ Entity anno and Hibernate constructor
-    //4. do something similar w/ Skill class
+
+    //TODO: add public accessor methods to Employer.
+
 }
